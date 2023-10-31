8f4cdcba2fbfd2292ea19c19e93e3d4706017674
NatthaphongS <sinsupharoek@gmail.com>
2023-10-31 17:48:23 +0700

    config tailwind and index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#1472E7",
+      primaryLight: "#1FA0E4",
+      primaryDark: "#03196B",
+      secondary: "#F59E0B",
+      secondaryLight: "#FBBF24",
+      secondaryDark: "#D97706",
+      graylight: "#D4D4D4",
+      grayDark: "#525252",
+      mybackground: "#FFFBF2",
+      darktext: "#171717",
+      whitetext: "#F8FAFC",
+      mygreen: "#22C55E",
+      red: "#EF4444",
+    },
+    fontWeight: {
+      light: "300",
+      normal: "400",
+      semibold: "500",
+      bold: "600",
+    },
   },
   plugins: [],
 };
