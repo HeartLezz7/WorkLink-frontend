a455b892c0fac35a291439753464990ecacbd450
NatthaphongS <sinsupharoek@gmail.com>
2023-11-02 12:46:03 +0700

    profilepage 20%

--- src/pages/userprofilepage/ProfilePage.jsx
+++ src/pages/userprofilepage/ProfilePage.jsx
@@ -1,20 +1,35 @@
+import UserDetail from "./UserDetail";
 export default function UserProfilePage() {
   return (
     <>
       <div className="absolute w-full h-[430px] bg-primaryLight z-[-5]"></div>
       <div className="max-w-[1440px] mx-auto">
         <div className="flex gap-10">
-          <div className="bg-black flex-[5] flex justify-center">
-            <div className="px-5 bg-secondaryLight">
-              <div className="rounded-full overflow-hidden w-fit">
+          <div className=" flex-[3] flex justify-center pt-[100px]">
+            <UserDetail />
+          </div>
+          <div className=" flex-[5] flex justify-center mt-[100px] p-5 bg-background/80 rounded-2xl">
+            <div className="flex gap-5 items-center justify-start w-[700px]  h-[250px] p-3 rounded-lg border-2 border-textGrayDark">
+              <div className="w-[200px] bg-primary/60 rounded-lg overflow-hidden h-[220px]">
                 <img
-                  src="/defaultImage.jpg"
-                  className="w-[150px] aspect-square object-cover"
+                  src="/showCase1.png"
+                  className="w-full h-[150px] object-cover"
                 />
+                <div className="p-3 flex justify-center items-center h-[70px]">
+                  <p className="line-clamp-2">title</p>
+                </div>
+              </div>
+              <div className="w-[200px] bg-background/80 rounded-lg overflow-hidden h-[220px]">
+                <img
+                  src="/showCase1.png"
+                  className="w-full h-[150px] object-cover"
+                />
+                <div className="p-3 flex justify-center items-center h-[70px]">
+                  <p className="line-clamp-2">title</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="bg-background flex-[10] w-full">dasljsl</div>
         </div>
       </div>
     </>
---
+++ src/pages/userprofilepage/UserDetail.jsx
@@ -0,0 +1,52 @@
+import { FaStar } from "react-icons/fa";
+
+export default function UserDetail() {
+  return (
+    <div className="p-5 w-[90%] shadow-lg shadow-primaryDark/70 bg-background/80 rounded-xl">
+      <div className="flex gap-5 items-center">
+        <div className="rounded-full overflow-hidden w-fit shadow-md">
+          <img
+            src="/defaultImage.jpg"
+            className="w-[150px] aspect-square object-cover"
+          />
+        </div>
+        <div className="space-y-1">
+          <div>
+            <h6 className="text-textGrayDark truncate">username</h6>
+            <h6 className="text-textGrayDark truncate">lastname</h6>
+          </div>
+          <div className="flex items-start gap-1">
+            <FaStar color="#FFC911" size={25} />
+            <p className="text-xl font-semibold text-textGrayDark">5.0/5.0</p>
+          </div>
+        </div>
+      </div>
+      <div className=" flex flex-col gap-3 mt-4">
+        <p className="text-xl">
+          <span className="text-xl font-semibold">Age :</span> 25 years
+        </p>
+        <p className="text-xl">
+          <span className="text-xl font-semibold">Address :</span> Bangkok,
+          Thailand
+        </p>
+        <div>
+          <p className="text-xl font-semibold">Personal description :</p>
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis
+            maxime, vel quibusdam beatae eos excepturi necessitatibus voluptatem
+            quidem sed praesentium exercitationem libero numquam dolores
+            reprehenderit assumenda, dignissimos nemo deleniti doloribus
+            reiciendis. Voluptates labore laboriosam eaque quasi facere? Ab
+            consequatur voluptatem nobis, ipsa praesentium ipsum mollitia eos
+            aspernatur. Animi, cupiditate nobis!
+          </p>
+        </div>
+        <div className="w-full flex justify-center">
+          <button className="bg-textGrayLight text-textGrayDark px-3 py-2 rounded-md font-semibold">
+            Edit profile
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
