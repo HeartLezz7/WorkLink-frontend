a639d999a47b415e88645f22b512281a245a3378
HeartLezz7 <heartchantapath@gmail.com>
2023-11-03 17:57:55 +0700

    finish ui chatroom

--- src/pages/chat/ChatBox.jsx
+++ src/pages/chat/ChatBox.jsx
@@ -1,12 +1,25 @@
+import plane from "../../../public/icons/plane.png";
+import plus from "../../../public/icons/plus.png";
+
 export default function ChatBox() {
   return (
-    <div className="grid grid-rows-6 ">
+    <div className="grid grid-rows-6 border-x-2 border-x-textGrayLight">
       <div className="row-span-5">
-        <div className="bg-secondaryLight text-textWhite text-4xl text-center p-3 font-semibold">
+        <div className="bg-primary text-textWhite text-4xl text-center p-3 font-semibold">
           Status
         </div>
       </div>
-      <div className="row-span-1">Chat Box</div>
+      <div className="row-span-1 p-10">
+        <div className="border flex justify-between items-center px-6 py-3 rounded-xl">
+          <div className="flex items-center gap-2">
+            <img src={plus} alt="plus" className="w-[40px]" />
+            <div>message...</div>
+          </div>
+          <div>
+            <img src={plane} alt="plane" className="w-[40px]" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/chat/ChatList.jsx
+++ src/pages/chat/ChatList.jsx
@@ -1,7 +1,49 @@
+import search from "../../../public/icons/search.png";
 export default function ChatList() {
   return (
     <div>
-      <div>Chat List</div>
+      <div className="p-5 border-b">
+        <div className="border flex justify-between items-center px-6 py-3 rounded-xl">
+          <div>Search for...</div>
+          <div>
+            <img src={search} alt="search" className="w-[30px]" />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col border-collapse">
+        <div className="w-full border border-t-0 flex gap-5 items-center px-2 py-5 ">
+          <img
+            src={search}
+            alt="search"
+            className="w-[100px] rounded-full border"
+          />
+
+          <div className="flex flex-col gap-1">
+            <div>John Doe</div>
+            <div>Work id : WLPT123456789</div>
+            <div className="flex justify-between">
+              <div>21 Oct 2023</div>
+              <div>Now...</div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full border border-t-0 flex gap-5 items-center px-2 py-5 ">
+          <img
+            src={search}
+            alt="search"
+            className="w-[100px] rounded-full border"
+          />
+
+          <div className="flex flex-col gap-1">
+            <div>John Doe</div>
+            <div>Work id : WLPT123456789</div>
+            <div className="flex justify-between">
+              <div>21 Oct 2023</div>
+              <div>Now...</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
