d40120b59182753dfb7855c99fc8a488ba35e062
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2023-11-09 15:50:58 +0700

    Nothing change

--- src/pages/userprofilepage/showcasedetail/ShowCaseCard.jsx
+++ src/pages/userprofilepage/showcasedetail/ShowCaseCard.jsx
@@ -4,7 +4,7 @@ export default function ShowCaseCard({showcase}) {
   console.log(showcase)
   return (
     <div className="w-[270px] h-[350px] bg-primary rounded-lg overflow-hidden ">
-    <img src={showcase.imagePictue} className="w-full max-h-[280px] object-cover" />
+    <img src={showcase.imagePicture} className="w-full max-h-[280px] object-cover" />
     <div className="p-3 flex justify-center items-center h-[70px]">
       <p className="line-clamp-2 text-textNavy font-semibold text-center">
         {showcase.description}
