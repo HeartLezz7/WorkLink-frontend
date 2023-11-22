4f792be5941fa3a6e5f7249dd45339f7c0c44b32
NatthaphongS <sinsupharoek@gmail.com>
2023-11-22 15:26:14 +0700

    fix redirect

--- src/router/redirect/RedirectIsAdmin.jsx
+++ src/router/redirect/RedirectIsAdmin.jsx
@@ -1,9 +1,9 @@
-import { Navigate } from "react-router-dom";
-import useAuth from "../../hooks/useAuth";
+import { Navigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 export default function RedirectIsAdmin({ children }) {
   const { user } = useAuth();
-  if (user?.authUser?.userType !== "admin") {
+  if (user?.authUser?.userType !== 'admin') {
     return <Navigate to={`/`} />;
   }
   return children;
